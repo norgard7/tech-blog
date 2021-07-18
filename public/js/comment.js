5d3f1d6610f443fc2dc7b8ecf40fb1300cbe00d0
Lauren Norgard <lauren.c.schumann@gmail.com>
2021-07-17 21:51:04 -0500

    added js to profile.js, updated handlebars for homepage. added code to my views

---
+++ public/js/comment.js
@@ -0,0 +1,46 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const comment = document.querySelector('#comment').value.trim();
+    const date = format_date();
+  
+    if (comment) {
+      const response = await fetch(`/api/comment`, {
+        method: 'POST',
+        body: JSON.stringify({ comment_content, post_id, user, date }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create comment');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/projects/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-project-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.project-list')
+    .addEventListener('click', delButtonHandler);

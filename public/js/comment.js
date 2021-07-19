bcb38ad792f170145c2cf8426530203ec3aa862f
Lauren Norgard <lauren.c.schumann@gmail.com>
2021-07-18 20:51:08 -0500

    tweaked the handlebars, to see if they can be deployed to heroku.

--- public/js/comment.js
+++ public/js/comment.js
@@ -3,6 +3,7 @@ const newFormHandler = async (event) => {
   
     const comment = document.querySelector('#comment').value.trim();
     const date = format_date();
+    const user = req.session.user_name;
   
     if (comment) {
       const response = await fetch(`/api/comment`, {
@@ -21,26 +22,9 @@ const newFormHandler = async (event) => {
     }
   };
   
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/projects/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to delete project');
-      }
-    }
-  };
+
   
   document
-    .querySelector('.new-project-form')
+    .querySelector('.new-comment-form')
     .addEventListener('submit', newFormHandler);
   
-  document
-    .querySelector('.project-list')
-    .addEventListener('click', delButtonHandler);
